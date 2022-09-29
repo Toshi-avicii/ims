450330c29d60490c36c02a162319f3a411539752
Amit <kumamit247@gmail.com>
2022-09-29 16:46:07 +0530

    add get route /api/admin/profile

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,20 @@ const app = express();
 const env = require('./config/envConfig');
 const cors = require('cors');
 const connect = require('./config/db');
-const authrouter = require('./routes/authRoute');
+const authRoute = require('./routes/authRoute');
 const counselorsRoute = require('./routes/counselorsRoute');
 const leadsRoute = require('./routes/leadsRoute');
+const adminRoute = require('./routes/adminRoute');
 
 // db connection 
 connect();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authrouter);
+app.use("/api", authRoute);
 app.use("/api/counselors", counselorsRoute);
 app.use("/api/leads", leadsRoute);
+app.use("/api/admin", adminRoute)
 
 const port = env.PORT || 6000;
 app.listen(port, () => {
